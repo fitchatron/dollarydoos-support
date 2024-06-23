4e139a036cb1ace8e824e890ff5f8112310d0073
fitchatron <53107122+fitchatron@users.noreply.github.com>
2024-06-24 09:45:05 +1000

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [vue()],
-	base: "/dollarydoos-support/",
-});
+	base: mode === "development" ? undefined : "/dollarydoos-support/",
+}));
